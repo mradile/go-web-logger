9b47838acfab83fc2530e64e8a9f6d9c3f1b62fe
Martin Radile <martin.radile@aleri.de>
2021-04-28 08:05:47 +0200

    handler for returning env var content and file

--- main.go
+++ main.go
@@ -33,6 +33,36 @@ func kill(w http.ResponseWriter, request *http.Request) {
 	log.Fatalf("killing %s", instanceID)
 }
 
+func env(w http.ResponseWriter, request *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	val := os.Getenv("MOUNT_FILE")
+	fmt.Fprintf(w, "env %s", val)
+	log.Infof("env %s", val)
+}
+
+func file(w http.ResponseWriter, request *http.Request) {
+
+	val := os.Getenv("MOUNT_FILE")
+	f, err := os.Open(val)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(w, "could not open file %s: %s", val, err)
+		log.Errorf("could not open file %s: %s", val, err)
+		return
+	}
+	data, err := ioutil.ReadAll(f)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(w, "could not read file %s: %s", val, err)
+		log.Errorf("could not read file %s: %s", val, err)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "file %s", data)
+	log.Infof("file %s", data)
+}
+
 func main() {
 	addr := os.Getenv("ADDR")
 	if addr == "" {
@@ -66,6 +96,8 @@ func main() {
 
 	http.HandleFunc("/ping", pong)
 	http.HandleFunc("/kill", kill)
+	http.HandleFunc("/env", env)
+	http.HandleFunc("/file", file)
 
 	log.Infof("starting http server, listening on %s", addr)
 	if err := http.ListenAndServe(addr, nil); err != nil {
